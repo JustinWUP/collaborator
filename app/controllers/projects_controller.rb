b24de43f715d30b5cfbb00510ea6e41dea918a94
Dan Schuman <danschuman@gmail.com>
2012-02-17 12:27:35 -0800

    Readme

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,8 @@ class ProjectsController < ApplicationController
 
   load_and_authorize_resource :through => :current_user
 
+  respond_to :html, :js
+
   # GET /projects
   # GET /projects.json
   def index
@@ -13,20 +15,13 @@ class ProjectsController < ApplicationController
       @projects = Project.all
     end
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @projects }
-    end
   end
 
 
   # GET /projects/1
   # GET /projects/1.json
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @project }
-    end
+
   end
 
   # GET /projects/new
@@ -34,10 +29,6 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
   end
 
   # GET /projects/1/edit
@@ -50,15 +41,19 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(params[:project])
 
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    @project.save
+
+    respond_with @project, :location => projects_url
+
+    # respond_to do |format|
+    #   if @project.save
+    #     format.html { redirect_to @project, notice: 'Project was successfully created.' }
+    #     format.json { render json: @project, status: :created, location: @project }
+    #   else
+    #     format.html { render action: "new" }
+    #     format.json { render json: @project.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PUT /projects/1
@@ -88,10 +83,12 @@ class ProjectsController < ApplicationController
     
     @project.destroy
 
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
-    end
+    respond_with @project, location: projects_url
+    # respond_to do |format|
+    #   format.html { redirect_to projects_url }
+    #   format.json { head :no_content }
+    #   format.js
+    # end
   end
 
   private
