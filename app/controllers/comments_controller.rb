8c1163af64b6b74d1662b6bcc4f1af6fc0d9adca
Dan Schuman <danschuman@gmail.com>
2012-03-08 19:41:03 -0800

    Comments work now..

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,16 @@
 class CommentsController < ApplicationController
 	respond_to :html, :js
 
-	load_and_authorize_resource :project, :through => :current_user
+	load_and_authorize_resource :project
 
 	before_filter :auth_issue
 
 	def create
 		# Make the comment...
-		@comment = Github::Comment.new
+		user, repo = @project.repo.split('/')
+		@comment = Github::Comment.new({gh_user: user, gh_repo: repo, issue_id: params[:issue_id]})
+		@comment.body = params[:github_comment][:body]
+		@comment.save
 
 		respond_with @comment, location: project_issue_path(params[:project_id], params[:issue_id])
 	end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
+require 'http_debug'
+
 Githubbug::Application.configure do
+  Net::HTTP.enable_debug!
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
