f30bac753914669c8726890338def6c8061dcbb4
justinwup <justin@winduppixel.com>
2012-03-10 02:09:01 -0600

    styled the email sent when you reset your password and the change my password button on the form you get to when you click the link in the mail.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "The WUP Robot Helper <robotassistant@winduppixel.com>"
+  config.mailer_sender = "The WUP Robot Helper <robothelper@winduppixel.com>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
