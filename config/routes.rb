567869c6fa7649d2e906ff9ef8eee1f93bf7880f
Dan Schuman <danschuman@gmail.com>
2012-05-11 13:02:00 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Githubbug::Application.routes.draw do
-  resources :comments
-
-  resources :topics
-
-    scope '/admin' do
-      resources :settings
-      resources :organizations
-    end
+  scope '/admin' do
+    resources :settings
+    resources :organizations
+  end
 
   resources :projects do
     # get 'topics/:topic_id', controller: "topics", action: "show", as: "topic"
