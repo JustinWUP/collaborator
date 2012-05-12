1b4c6040f10e84823d59a74cf5b805b33fbb9b90
justinwup <justin@winduppixel.com>
2012-05-12 01:28:05 -0500

    adjusting how hours fields are validated

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,6 @@ class Topic < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
   has_many :comments
+  validates :hoursreq, :numericality => { :greater_than_or_equal_to => 0 } 
+  validates :hoursused, :numericality => { :greater_than_or_equal_to => 0 } 
 end
