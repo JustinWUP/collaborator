66620e42ecfab8a5097f51f18af32b4e85e3b38b
Dan Schuman <danschuman@gmail.com>
2012-06-15 15:53:37 -0700

    Topic read permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,8 +9,9 @@ class Ability
       else
         can :read, Project, :users => {:id => user.id }
         can :create, Topic
+        can :read, Topic
         can :manage, Subscription, :user_id => user.id
-        can :create, :comment 
+        can :create, Comment 
         cannot :all, Setting
       end
 
