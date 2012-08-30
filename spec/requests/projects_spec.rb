7d84241e4e7a0daf12fe0fe368578b7027a395e3
Dan Schuman <danschuman@gmail.com>
2012-08-30 16:18:23 -0700

    Clean up specs

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -2,17 +2,4 @@ require 'spec_helper'
 
 describe "Projects" do
 
-	describe "GET /projects" do
-		before(:all) do
-			# TODO: Should be FactoryGirl
-			u = User.new(email: 'user@example.com', password: 'password', registration_org_id: 872872872 )
-			visit(new_user_session_path)
-			
-		end
-
-		it "works!" do
-	  		get projects_path
-			response.status.should be(200)  
-		end
-	end
 end
