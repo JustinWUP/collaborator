f834127610751321acccfc258760bb099a5d69b9
justinwup <justin@winduppixel.com>
2012-09-16 11:16:46 -0500

    changed action_mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,15 +24,24 @@ Collaborator::Application.configure do
 
   
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'winduppixel.com',
+  #   :user_name            => 'mailer@winduppixel.com',
+  #   :password             => 'K8|;;/fv*x=.W^v',
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true
+  # }
+
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "mail.winduppixel.com",
     :port                 => 587,
     :domain               => 'winduppixel.com',
     :user_name            => 'mailer@winduppixel.com',
-    :password             => 'K8|;;/fv*x=.W^v',
+    :password             => 'p~#4%(KF3)%t>:~',
     :authentication       => 'plain',
-    :enable_starttls_auto => true
-  }
+    :enable_starttls_auto => true }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
