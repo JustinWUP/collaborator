0bb0c3c3aa9419a891e00a73049b71e0e4566a7a
justinwup <justin@winduppixel.com>
2012-09-17 00:58:15 -0500

    updated sorting for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -9,9 +9,9 @@ class Topic < ActiveRecord::Base
   # scope :active, :conditions => { :topiccomplete => false }
   # scope :complete, :conditions => { :topiccomplete => true, :awaitingapproval => false }
   # scope :topic_for_approval, :conditions => {:awaitingapproval => true }
-  scope :active, :conditions => {:work_status => 1}
-   scope :topic_for_approval, :conditions => {:work_status => 2}
-  scope :complete, :conditions => {:work_status => 3}
+  scope :active, :conditions => {:work_status => 1}, :order => 'created_at DESC'
+   scope :topic_for_approval, :conditions => {:work_status => 2} , :order => 'updated_at DESC'
+  scope :complete, :conditions => {:work_status => 3}, :order => 'updated_at DESC'
 
   has_many :subscriptions, :as => :subscribable, :dependent => :destroy
   validates :hoursreq, :numericality => { :greater_than_or_equal_to => 0 } 
