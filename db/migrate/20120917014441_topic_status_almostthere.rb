c5fb585c595d2ea0af765d62efdb2f8ca4f87754
justinwup <justin@winduppixel.com>
2012-09-17 00:34:39 -0500

    activerecord is annoying

--- db/migrate/20120917014441_topic_status_almostthere.rb
+++ db/migrate/20120917014441_topic_status_almostthere.rb
@@ -1,6 +1,5 @@
 class TopicStatusAlmostthere < ActiveRecord::Migration
   def change
   	change_column :topics, :work_status, :integer, :default => 1
-	remove_column :topics, :topic_status
   end
 end
--- db/migrate/20120917052356_doyouhaveproblem.rb
+++ db/migrate/20120917052356_doyouhaveproblem.rb
@@ -1,6 +1,5 @@
 class Doyouhaveproblem < ActiveRecord::Migration
 def change
-	remove_column :topics, :topic_status
 
 end
 end
