502f65ce7018499d29feb9a4e7020b678cd0fd1c
justinwup <justin@winduppixel.com>
2012-09-29 01:17:45 -0500

    another hack to fix subscriptions table

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -46,9 +46,10 @@ class SubscriptionsController < ApplicationController
   end
 
   def filter_index
-    #@subscriptions = Subscription.accessible_by(current_ability).select {|obj| obj.enabled == true}
+    @subscriptions = Subscription.accessible_by(current_ability).select {|obj| obj.enabled == true}
 
     # HACK: Workaround for Topics without Projects
-    #@subscriptions = @subscriptions.reject {|obj| obj.subscribable.project == nil}
+    @subscriptions = @subscriptions.reject {|obj| obj.subscribable.project.name == nil}
+    @subscriptions = @subscriptions.reject {|obj| obj.subscribable.title == nil}
   end
 end
