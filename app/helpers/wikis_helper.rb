dd0d06f2751df4fa71e34935380215af20a60c72
justinwup <justin@winduppixel.com>
2012-10-07 02:11:59 -0500

    welp I guess I have to change @ to ~ for shortcodes

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,6 +1,6 @@
 module WikisHelper
 	def wiki_body(content)
-		BlueCloth.new(content.gsub('@@','/wikis/').gsub('@','/wikis/cate/')).to_html.html_safe
+		BlueCloth.new(content.gsub('~~','/wikis/').gsub('~','/wikis/cate/')).to_html.html_safe
 	end
 
 	 def category_list(category)
