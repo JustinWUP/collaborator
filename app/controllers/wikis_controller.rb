a340c9ef67891840dc0818ce5e44271e7b916985
justinwup <justin@winduppixel.com>
2012-10-08 13:04:51 -0500

    heroku hacks

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -17,7 +17,7 @@ class WikisController < ApplicationController
   end
 
 def archives
-  @wikiarchive = Wiki.paginate(:page => params[:page], :per_page => 10, :conditions => "title!='cate'", :order => "UPPER(title) ASC")
+  @wikiarchive = Wiki.paginate(:page => params[:page], :per_page => 10, :conditions => "slug!='cate'", :order => "UPPER(title) ASC")
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @wikis }
@@ -25,7 +25,7 @@ def archives
 end
 
 def catearchives
-  @catearchive = Wiki.paginate(:page => params[:page], :per_page => 10, :group => "postcategory", :conditions => "postcategory!='/wiki'", :order => "UPPER(postcategory) ASC")
+  @catearchive = Wiki.paginate(:page => params[:page], :per_page => 10, :group => {"wikis.id", "postcategory"}, :conditions => "postcategory!='/wiki'", :order => "UPPER(postcategory) ASC")
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @wikis }
