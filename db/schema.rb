1214bddaba0bd7965e3ddc5c54d9bc34605622aa
Justin Bean <justin@winduppixel.com>
2012-11-18 13:25:25 -0600

    switch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121118014529) do
+ActiveRecord::Schema.define(:version => 20121118084041) do
 
   create_table "apps", :force => true do |t|
     t.string   "title"
@@ -66,12 +66,6 @@ ActiveRecord::Schema.define(:version => 20121118014529) do
     t.integer  "user_id"
   end
 
-  create_table "conversations", :force => true do |t|
-    t.string   "subject",    :default => ""
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-  end
-
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
     t.integer  "attempts",   :default => 0
@@ -96,24 +90,6 @@ ActiveRecord::Schema.define(:version => 20121118014529) do
     t.datetime "updated_at",                    :null => false
     t.boolean  "enabled",    :default => false
   end
-
-  create_table "notifications", :force => true do |t|
-    t.string   "type"
-    t.text     "body"
-    t.string   "subject",              :default => ""
-    t.integer  "sender_id"
-    t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                :default => false
-    t.datetime "updated_at",                              :null => false
-    t.datetime "created_at",                              :null => false
-    t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "notification_code"
-    t.string   "attachment"
-  end
-
-  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
@@ -142,20 +118,6 @@ ActiveRecord::Schema.define(:version => 20121118014529) do
     t.integer "user_id"
     t.integer "project_id"
   end
-
-  create_table "receipts", :force => true do |t|
-    t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                                  :null => false
-    t.boolean  "read",                          :default => false
-    t.boolean  "trashed",                       :default => false
-    t.boolean  "deleted",                       :default => false
-    t.string   "mailbox_type",    :limit => 25
-    t.datetime "created_at",                                       :null => false
-    t.datetime "updated_at",                                       :null => false
-  end
-
-  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -188,11 +150,11 @@ ActiveRecord::Schema.define(:version => 20121118014529) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
-    t.string   "time"
+    t.string   "time",        :default => "0.0"
     t.text     "description"
     t.boolean  "active"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
     t.integer  "topic_id"
     t.boolean  "billable"
   end
@@ -215,10 +177,6 @@ ActiveRecord::Schema.define(:version => 20121118014529) do
     t.integer  "percentcomplete"
     t.integer  "work_status",             :default => 1
     t.string   "topic_tag",               :default => "New Topic,newtopictag"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "attachment_file_name"
     t.string   "attachment_content_type"
     t.integer  "attachment_file_size"
@@ -241,10 +199,6 @@ ActiveRecord::Schema.define(:version => 20121118014529) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.integer  "organization_id"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.boolean  "blocked",                :default => false
     t.boolean  "topicmail",              :default => true
     t.string   "firstname",              :default => ""
