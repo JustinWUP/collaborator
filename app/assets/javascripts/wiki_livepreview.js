96e16f66062ffb5eaccab54e8d2695b0dfa8bb5b
justinwup <justin@winduppixel.com>
2012-12-04 17:02:10 -0600

    fixed the wikis form so you cannot leave until you save the article

--- app/assets/javascripts/wiki_livepreview.js
+++ app/assets/javascripts/wiki_livepreview.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-	x = "<%= @wiki.title.downcase %>/";
+	x = $('#titlemunch').text() + '/';
   btn_clicked = false;
-  burger = false;
+  changed = false;
 
 
   $('input[type="submit"]').click(function(){
@@ -12,31 +12,40 @@ $(document).ready(function(){
 	converter = new Markdown.Converter()
 
 	if( $('#wiki_body').val() != '' ) {
-  		$('.wiki_preview').empty().append(converter.makeHtml($('#wiki_body').val()))
+  		$('.wiki_preview').empty().append(converter.makeHtml($('#wiki_body').val()));
   		linkfixer();
 	}
     $('#wiki_body').keyup(function(){
     	$('.wiki_preview').empty().append(converter.makeHtml($('#wiki_body').val()));
-      burger = true;
+      changed = true;
     	linkfixer();
     });
       
 	function linkfixer(){
-	$('.wiki_preview a').each(function(){
-		if(this.href.match("~~")){
-    		link = this.href.replace('~~', '');
-  		}
-  
- 		else if(this.href.match("~")){
-    		link = this.href.replace('~', 'cate/');
-  		}
+  	env = ['ghb', 'collaborator', 'localhost']
+    $('.wiki_preview a').each(function(){
+  		
+      if(!this.href.match(env.hash)){
+        this.href=this.href;
+      }
+      else{
 
-  		this.href = link.replace(x, '');
+      if(this.href.match("~~")){
+      		link = this.href.replace('~~', '');
+    		}
+    
+   		else if(this.href.match("~")){
+      		link = this.href.replace('~', 'cate/');
+    		}
+
+    	this.href = link.replace(x, '');
+    }
+
     });
-	}
+  }
 
     window.onbeforeunload = function(){
-      if(btn_clicked == false && burger == true) {
+      if(btn_clicked == false && changed == true) {
               return "Don't forget to save your article!";
       }
     }
