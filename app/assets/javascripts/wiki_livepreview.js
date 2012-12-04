6e408a9f419a1763e9abff7228cb828e63710223
justinwup <justin@winduppixel.com>
2012-12-04 16:06:57 -0600

    fixed the wikis form so you cannot leave until you save the article

--- app/assets/javascripts/wiki_livepreview.js
+++ app/assets/javascripts/wiki_livepreview.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
+
 	x = "<%= @wiki.title.downcase %>/";
-	
+  btn_clicked = false;
+  burger = false;
+
+
+  $('input[type="submit"]').click(function(){
+    btn_clicked = true; 
+  });
+
 	converter = new Markdown.Converter()
+
 	if( $('#wiki_body').val() != '' ) {
   		$('.wiki_preview').empty().append(converter.makeHtml($('#wiki_body').val()))
   		linkfixer();
 	}
     $('#wiki_body').keyup(function(){
-    	$('.wiki_preview').empty().append(converter.makeHtml($('#wiki_body').val()))
+    	$('.wiki_preview').empty().append(converter.makeHtml($('#wiki_body').val()));
+      burger = true;
     	linkfixer();
     });
       
@@ -24,4 +34,11 @@ $(document).ready(function(){
   		this.href = link.replace(x, '');
     });
 	}
+
+    window.onbeforeunload = function(){
+      if(btn_clicked == false && burger == true) {
+              return "Don't forget to save your article!";
+      }
+    }
+
 });
