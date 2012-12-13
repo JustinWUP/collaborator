b496affd0270c86bdd5b0d452a388cd4d76240f0
justinwup <justin@winduppixel.com>
2012-12-12 20:59:39 -0600

    fixed a bug with subscription emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -61,7 +61,7 @@ class Notifier < ActionMailer::Base
     resource_name = resource.class.to_s.downcase 
     
     helper = Rails.application.routes.named_routes.helpers.grep(/.*#{resource_name}_path$/).first.to_s.split('_')
-    built = helper.slice!(-2,2) # Shortest possible valid helper, "comment_path"
+    built = helper.slice!(-2,3) # Shortest possible valid helper, "comment_path"
     
     while !(respond_to?(built.join("_").to_sym)) 
       built.unshift helper.pop
